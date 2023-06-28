8b3bc559ceb51dcd16fa0c8ab7badf22655931bf
Screetract Solutions <work@screetract.com>
2023-06-28 16:43:20 +0530

    Code update for the JSON Server integration

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,29 +1,53 @@
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import { Link } from 'react-router-dom';
-import '../../styles/Home.scss'
-import { useDispatch, useSelector } from 'react-redux';
-import { increment, decrement } from '../../globalState/storeSlices/counterSlice';
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import { Link } from "react-router-dom";
+import "../../styles/Home.scss";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  increment,
+  decrement,
+} from "../../globalState/storeSlices/counterSlice";
+import { useEffect } from "react";
 
 const Home = () => {
-  
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
-
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  // sample API call from the JSON-Server
+  useEffect(() => {
+    fetch("http://localhost:5173/api/products")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
+  // API Call code End
   return (
-    <div className='home'>
-      <Button variant="outlined" >Hello world! - let build the fourth Unicorn 🚀</Button>
+    <div className="home">
+      <Button variant="outlined">
+        Hello world! - let build the fourth Unicorn 🚀
+      </Button>
       <Link to="/login">
-        <Button variant="contained" endIcon={<SendIcon />} >Go to Login</Button>
+        <Button variant="contained" endIcon={<SendIcon />}>
+          Go to Login
+        </Button>
       </Link>
       {/* create a counter with increment and decrement button */}
-      <Button variant="contained" color="success" onClick={() => dispatch(increment())} >Increment</Button>
+      <Button
+        variant="contained"
+        color="success"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </Button>
       {/* text for counter */}
       <p>{count}</p>
-      <Button variant="contained" color="error" onClick={() => dispatch(decrement())} >Decrement</Button>
-
+      <Button
+        variant="contained"
+        color="error"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import jsonServer from "vite-plugin-simple-json-server";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    jsonServer({
+      urlPrefixes: ["/api/"],
+      mockDir: "mock-api",
+      handlers: [
+        {
+          pattern: "/api/home",
+          method: "GET",
+          handle: (req, res) => {
+            const data = {
+              data: "This is Json-Server running directly on Vite",
+            };
+            res.setHeader("content-type", "application/json");
+            res.end(JSON.stringify(data));
+          },
+        },
+      ],
+    }),
+  ],
+});
